8624ebe0d0fa0a030583048f074c17a9156fe0c7
dudziec <dudziecedamian@gmail.com>
2019-10-03 12:12:46 +0200

    Implemented methods for shuffling the deck and checking if deck contains a card

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,18 +1,21 @@
 defmodule Cards do
-  @moduledoc """
-  Documentation for Cards.
-  """
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five", "Six"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
 
-  @doc """
-  Hello world.
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
+  end
 
-  ## Examples
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
 
-      iex> Cards.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
   end
 end
